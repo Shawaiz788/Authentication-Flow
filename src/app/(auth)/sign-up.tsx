b735e66fe7ccb813594b6010f2bde60ce2a46cef
Shawaiz Ali Rehman <shawaiali788@gmail.com>
2026-07-01 12:56:25 +0800

    Added:email verification- through code

--- src/app/(auth)/sign-up.tsx
+++ src/app/(auth)/sign-up.tsx
@@ -11,7 +11,7 @@ import CustomButton from '@/components/CustomButton';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
 
 import { useSignUp } from '@clerk/clerk-expo';
 
@@ -39,6 +39,8 @@ export default function SignUpScreen() {
                 emailAddress: data.email,
                 password: data.password,
             });
+            await signUp.prepareVerification({ strategy: 'email_code' })
+            router.push('/verify')
         } catch (error) {
             console.log('Sign up error: ', error);
         }
